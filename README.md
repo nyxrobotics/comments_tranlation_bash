bb393442aa5092f7e8562d8cad05591077938ef5
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2022-09-12 15:59:23 +0900

    Translate launch files

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # comments_tranlation_bash
+## Description
+Scripts to translate ROS packages into English when they contain Chinese
+
+## Setup
+
+- Install tranlate-shell from Gitub
+```bash
+mkdir -p ~/lib && cd ~/lib
+git clone https://github.com/soimort/translate-shell
+cd translate-shell/
+make
+sudo chekinstall --pkgname=translate-shell --pkgversion=0.9.7
+```
+
+## Reference
+- Check if Full-size characters exsist : https://hacknote.jp/archives/33123/
+- grep -q option : https://genzouw.com/entry/2020/02/10/090005/1922/
---
+++ modules/ch2en_launch.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Translate launch files
+for i in $(find ./ -name '*.launch')
+do
+    firstline=$(head -n 1 $i)
+    if grep -q -n -v '<?xml' <<< $firstline ; then
+        echo "Add xml definition"
+        sed -i '1s/^/<?xml version="1.0" encoding="UTF-8"?>\n/' $i
+    fi
+    > $i.comments # Find the commented-out section in the file.
+    sed -i "s/<!-- -\*- mode: XML -\*- -->//g" $i
+    sed -i "s/<!-- \+/<!-- /g;s/ \+-->/ -->/g" $i
+    awk '/<!--/,/-->/' $i > $i.comments
+    sed -i "s/[\r\n]\+//g" $i.comments
+    sed -i "s/<!--/\n<!--/g;s/-->/-->\n/g" $i.comments
+    # awk -i inplace '/<!--/,/-->/' $i.comments
+    awk '/<!--/,/-->/' $i.comments > tmp && mv tmp $i.comments
+    while read single_comment;
+    do
+        raw_format=$(echo $single_comment | sed -e 's/ *$//g;s/<!--//g;s/-->//g;s/[]\/$*.^[]/\\&/g')
+        # Translate when comment-out contains double-byte characters
+        if LANG=C grep -q -n -v '^[[:cntrl:][:print:]]*$' <<< $raw_format ; then
+            en_escape=$(echo $(trans -brief -no-warn -no-ansi zh-CN:en $raw_format))
+            raw_escape=$(echo $single_comment | sed -e 's/[]\/$*.^[]/\\&/g')
+            sed -i -z "s/$raw_escape/<!-- ${en_escape^} -->/g" $i
+            echo -e $raw_escape "\n<<translated>>\n" ${en_escape^} "\n-----"
+        fi
+    done < $i.comments # Translate all comment-outs
+    rm $i.comments
+done
